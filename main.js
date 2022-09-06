8f97a68a79536ef16dfc19a50c64d72b8c9bb07e
Naman-3010 <91064945+Naman-3010@users.noreply.github.com>
2022-09-06 10:16:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,45 @@
 img="";
 
 function preload(){
-    img=loadImage('Vishnu.jpg');
+    img=loadImage('Elephant.jpg');
 }
 
 function setup(){
     canvas=createCanvas(600,400);
     canvas.center();
+    object_detector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detecting objects";
+
+}
+
+function modelLoaded(){
+    console.log("modelLoaded");
+    status=true;
+    object_detector.detect(img,gotResult);
+
+
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    
 
 }
 
 function draw(){
     image(img,0,0,600,400);
     fill("red");
-    text("god",45,75);
+    text("baby elephant",45,190);
     noFill();
     stroke("brown");
-    rect(30,60,450,300);
+    rect(25,200,300,200);
+    text("Adult elephant",320,120);
+    fill("blue");
+    noFill();
+    stroke("yellow");
+    rect(300,90,200,450);
 
 }
